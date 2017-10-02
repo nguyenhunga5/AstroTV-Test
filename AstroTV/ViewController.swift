5a0aedd480d9d265f486b7dfc1c0a76bcdbf0ef3
Hung Nguyen Thanh <nguyenthanhhung19872004@gmail.com>
2017-10-02 21:25:32 +0700

    Initial Commit

---
+++ AstroTV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AstroTV
+//
+//  Created by Hung Nguyen Thanh on 10/2/17.
+//  Copyright © 2017 Hung Nguyen Thanh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
